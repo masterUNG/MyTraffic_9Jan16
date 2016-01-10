d3382c7d190b65932eb98e0a14f2c378cd6e8ba9
masterUNG <phrombutr@gmail.com>
2016-01-10 14:06:21 +0700

    Show View Finish

--- app/src/main/java/appewtc/masterung/mytraffic/DetailActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/DetailActivity.java
@@ -1,14 +1,53 @@
 package appewtc.masterung.mytraffic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView titleTextView, detailTextView;
+    private ImageView trafficImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        //Bind Widget
+        bindWidget();
+
+        //Show View
+        showView();
+
     }   // Main Method
+
+    private void showView() {
+
+        //Show Title
+        String strTitle = getIntent().getStringExtra("Title");
+        titleTextView.setText(strTitle);
+
+        //Show Image
+        int intIcon = getIntent().getIntExtra("Image", R.drawable.traffic_01);
+        trafficImageView.setImageResource(intIcon);
+
+        //Show Detail
+        int intIndex = getIntent().getIntExtra("Index", 0);
+        String[] detailStrings = getResources().getStringArray(R.array.detail_long);
+        detailTextView.setText(detailStrings[intIndex]);
+
+    }   // showView
+
+    private void bindWidget() {
+
+        titleTextView = (TextView) findViewById(R.id.txtShowTitle);
+        detailTextView = (TextView) findViewById(R.id.txtShowDetail);
+        trafficImageView = (ImageView) findViewById(R.id.imvShowImage);
+
+    }
 
 }   // Main Class
